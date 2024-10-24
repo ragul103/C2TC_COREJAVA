b9a1c9bfa23f1fe727e3cdb3daee0116d4b8e94a
rglra <rglra@RGLRAGUL>
2024-10-24 12:45:57 +0530

    First commit

---
+++ C2TC_RAGUL/src/com/tns/ifet/day1/FirstProgram.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.tns.ifet.day1;
+
+/**
+ * 
+ */
+public class FirstProgram {
+
+	/**
+	 * 
+	 */
+	public FirstProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+
+	}
+
+}
